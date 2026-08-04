8ae553418d244099f021d77306583a5b3fda4a91
Aditya Singh <jr.aditya004@gmail.com>
2026-08-04 10:39:12 +0530

    Add: house-robber - submission-5

---
+++ Data Structures & Algorithms/house-robber/submission-5.py
@@ -0,0 +1,16 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        n = len(nums)
+        prev = nums[0]
+        prev2 = 0
+        for index in range(1, n):
+            if index > 1:
+                pick = nums[index] + prev2
+            else:
+                pick = nums[index]
+            notpick = 0 + prev
+            curr = max(pick, notpick)
+            prev2 = prev
+            prev = curr
+            
+        return sol
